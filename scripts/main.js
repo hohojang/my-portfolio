40ff5d722da3671356deb4ed7df82defc40d7f3b
Park Jang Ho <96179658+hohojang@users.noreply.github.com>
2024-09-25 16:57:33 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -63,9 +63,12 @@ document.addEventListener('DOMContentLoaded', function() {
     // 타이핑 효과 (한국어)
     new Typed('#typing-text', {
         strings: [
-            "안녕하세요! 박장호입니다.",
-            "저는 매일 성장하는<br>개발자입니다.",
-            "개발에 매료되어<br>매일 노력하고 있습니다!"
+            "안녕하세요! 제 이름은 박장호입니다.",
+            "저는 의사소통 능력이 뛰어난<br>신입 개발자입니다.",
+            "항상 배우려는 자세로<br>끊임없이 성장하고 있습니다.",
+            "저의 가능성을 알아봐 주실<br>회사를 찾고 있습니다.",
+            "함께 발전해 나갈 수 있는<br>기회를 주시면 감사하겠습니다.",
+            "아래 Contact Information에서<br>연락처를 확인해 주세요!"
         ],
         typeSpeed: 50,
         backSpeed: 30,
@@ -85,8 +88,8 @@ document.addEventListener('DOMContentLoaded', function() {
         educationContent.innerHTML = `
             <h3>Education</h3>
             <ul>
-                <li>Bachelor's Degree in Computer Science, XYZ University (2018-2022)</li>
-                <li>High School Diploma, ABC High School (2015-2018)</li>
+                <li>목원대학교 지능로봇공학과 졸업 (2018년 03월 - 2024년 02월)</li>
+                <li>대전지족고등학교 졸업 (2014년 - 2017년)</li>
             </ul>
         `;
         certificationsContent.style.display = 'none';
@@ -97,9 +100,15 @@ document.addEventListener('DOMContentLoaded', function() {
         certificationsContent.innerHTML = `
             <h3>Certifications</h3>
             <ul>
-                <li>AWS Certified Developer - Associate (2023)</li>
-                <li>Microsoft Certified: Azure Fundamentals (2022)</li>
-                <li>CompTIA A+ (2021)</li>
+                <li>IOT지식능력검정자격증 (2023년 12월 08일)</li>
+                <li>자동차운전면허증 2종 보통 (2017년 12월 22일)</li>
+            </ul>
+            <h3>Training Completion</h3>
+            <ul>
+                <li>DSC공유대학 Amazon DeepRacer 자율주행교육 (2023년 11월 24일 ~ 2023년 11월 24일)</li>
+                <li>(주)새온 생산파트 인턴십 (2022년 11월 ~ 2023년 02월)
+                    <p>활동내용: 제품의 펌웨어를 업데이트 및 코드를 삽입하고 조향, 센서들의 이상 유무를 파악하고 점검과 교체를 하는 업무를 담당</p>
+                </li>
             </ul>
         `;
         educationContent.style.display = 'none';
